ffef1f658eb6d6faa0eedff014f5ac7d2a86161b
mbrodniak <michal.brodniak@partner.cgm.com>
2023-08-19 16:22:46 +0200

    fix

--- src/main/java/pl/coderslab/App.java
+++ src/main/java/pl/coderslab/App.java
@@ -1,11 +1,6 @@
 package pl.coderslab;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Component;
 import pl.coderslab.beans.HelloWorld;
 import pl.coderslab.config.ApplicationConfig;
 
@@ -19,8 +14,7 @@ public class App {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 
 		HelloWorld helloWorld = context.getBean("helloWorld", HelloWorld.class);
-		helloWorld.localDateAndTime();
-
+		helloWorld.hello();
 
 		context.close();
 	}
--- src/main/java/pl/coderslab/beans/HelloWorld.java
+++ src/main/java/pl/coderslab/beans/HelloWorld.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class HelloWorld {
 
-	public void	localDateAndTime(){
+	public void hello(){
 		System.out.println(LocalDateTime.now());
 	}
 }
